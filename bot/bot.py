826c4a033b81f01036093ccdc51b870b3c94a231
Jake Lyons <Vouch@live.co.uk>
2016-03-17 15:20:13 +0000

    Adjusted Some Things
    Separated location aiml into two files
    (location_directions.aiml and location_about.aiml)
    Also kept the old one for backup but this isn't used
    
    Added a way to test all responses with a command
    although this requires us to manually put the responses in ourselves in
    bot.py
    
    Fixed the save command; it now asks for a file name for a new brain file
    if none were selected at the beginning

--- bot/bot.py
+++ bot/bot.py
@@ -3,15 +3,14 @@ import os
 
 kernel = aiml.Kernel()
 
-
 print ("Choose a brain file:")
 print ("[1] Standard")
-print ("[2] Test")
+print ("[2] Project")
 print ("[3] Both")
 print ("[4] None\n")
 
 invalidSelection = True
-path = ""
+path = "notafile.brn"
 
 while invalidSelection:
     
@@ -20,35 +19,35 @@ while invalidSelection:
     inp = input(": ")
     
     if inp == "1":
-        path = "brains\\standard.brn"
+        path = "brains\\_standard.brn"
         
         if os.path.isfile(path):
             kernel.bootstrap(brainFile = path)
         else:
-            kernel.bootstrap(learnFiles = "std-all.xml", commands = "load aiml b")
+            kernel.bootstrap(learnFiles = "std-standard.xml", commands = "load aiml b")
             kernel.saveBrain(path)
 
         invalidSelection = False
     
     elif inp == "2":
-        path = "brains\\test.brn"
+        path = "brains\\_project.brn"
         
         if os.path.isfile(path):
             kernel.bootstrap(brainFile = path)
         else:
-            kernel.bootstrap(learnFiles = "std-test.xml", commands = "load aiml b")
+            kernel.bootstrap(learnFiles = "std-project.xml", commands = "load aiml b")
             kernel.saveBrain(path)
 
         invalidSelection = False
     
     elif inp == "3":
-        path = "brains\\both.brn"
+        path = "brains\\_both.brn"
         
         if os.path.isfile(path):
             kernel.bootstrap(brainFile = path)
         else:
-            kernel.bootstrap(learnFiles = "std-test.xml", commands = "load aiml b")
-            kernel.bootstrap(learnFiles = "std-all.xml", commands = "load aiml b")
+            kernel.bootstrap(learnFiles = "std-project.xml", commands = "load aiml b")
+            kernel.bootstrap(learnFiles = "std-standard.xml", commands = "load aiml b")
             kernel.saveBrain(path) 
         
         invalidSelection = False
@@ -58,34 +57,99 @@ while invalidSelection:
         invalidSelection = False
 
     elif inp == "5":
-        kernel.bootstrap(learnFiles = "std-test.xml", commands = "load aiml b")
+        print(";)")
+        kernel.bootstrap(learnFiles = "std-project.xml", commands = "load aiml b")
+        
         invalidSelection = False
 
 
 
 class Commands:
-    
+
+    def __init__(self, path):
+        self.path = path
+        
     def command_ping(self):
+        # Tests responsiveness
         print("ping")
     
     def command_quit(self):
+        # Quits the bot
         exit(0)
         
     def command_learntest(self):
-        kernel.bootstrap(learnFiles = "std-test.xml", commands = "load aiml b")
+        # Loads the aiml files for the project folder
+        kernel.bootstrap(learnFiles = "std-project.xml", commands = "load aiml b")
         
     def command_learnstandard(self):
-        kernel.bootstrap(learnFiles = "std-all.xml", commands = "load aiml b")
+        # Loads the aiml files for the standard folder
+        kernel.bootstrap(learnFiles = "std-standard.xml", commands = "load aiml b")
             
     def command_save(self):
-        if os.path.isfile(path): 
-            kernel.saveBrain(path)
-
+        # Saves the currently loaded aiml files to the brain file
+        # assuming one was selected at the beginning)
+        # if not then you are asked to name the new brain file
+        
+        if os.path.isfile(self.path): 
+            kernel.saveBrain(self.path)
+        else:
+            print("Name new brain file")
+            inp = input(": ")
+            self.path = "brains\\%s.brn" % inp
+            kernel.saveBrain(self.path)
+            
+            
+    def command_tlr(self):
+        # Test location responses
+        
+        inp = ""
+        for question in questionList:
+            print(question)
+            print(kernel.respond(question))
+            print()
+                                      
     def command_(self):
         bot_response = kernel.respond(message)
 
-c = Commands()
+c = Commands(path)
 
+locationList = [
+    "Student Union",
+    "Minerva Building",
+    "Main Building"
+    ]
+
+questionList = []
+for location in locationList:
+    # Directions
+    questionList.append("Directions %s" % location)
+    questionList.append("Can you tell me directions %s" % location)
+    
+    questionList.append("Directions to %s" % location)
+    questionList.append("Can you tell me directions to %s" % location)
+    
+    questionList.append("Directions for %s" % location)
+    questionList.append("Can you tell me directions for %s" % location)
+    
+    questionList.append("Get to %s" % location)
+    questionList.append("Can you tell me how to get to %s" % location)
+    
+    questionList.append("Where is %s" % location)
+    questionList.append("Hey, where is %s" % location)
+    questionList.append("Can you tell me where %s is" % location)
+    
+    questionList.append("Where's %s" % location)
+    questionList.append("Hey, where's %s" % location)
+
+    # About
+    questionList.append("About %s" % location)
+    questionList.append("Can you tell me about %s" % location)
+    
+    questionList.append("What is %s" % location)
+    questionList.append("Hey, what is %s" % location)
+    questionList.append("Can you tell me what %s is" % location)
+
+    
 while True:
     message = input(">>: ")
     try:
