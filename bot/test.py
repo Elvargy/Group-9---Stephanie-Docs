a08a12df01b293c92ae6055b57e1073bc32ec5fb
Jake Lyons <Vouch@live.co.uk>
2016-05-02 15:43:14 +0100

    Removed Fallthrough from Test + Basic Chat Changes

--- bot/test.py
+++ bot/test.py
@@ -6,12 +6,6 @@ import random
 kernel = aiml.Kernel()
 kernel.bootstrap(learnFiles = "std-project.xml", commands = "load aiml b")
 
-fallback = [
-    "Sorry I didn't understand that, could you please clarify your question?",
-    "I don't understand",
-    "Suck my dick you pixie wanker"
-    ]
-
 while True:
     inp = input(":> ")
     response = kernel.respond(inp)
@@ -19,7 +13,7 @@ while True:
     if response != "":
         print(response)
     else:
-        print(random.choice(fallback))
+        print("No message found")
     
 
 
